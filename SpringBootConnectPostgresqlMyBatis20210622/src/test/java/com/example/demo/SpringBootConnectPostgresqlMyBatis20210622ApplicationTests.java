bdcb907606599d81cee173bad7c5ca725cfbabcc
adachiy16 <yojiro.adadadachi@gmail.com>
2021-06-22 15:32:49 +0900

    initial commit

---
+++ SpringBootConnectPostgresqlMyBatis20210622/src/test/java/com/example/demo/SpringBootConnectPostgresqlMyBatis20210622ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConnectPostgresqlMyBatis20210622ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
